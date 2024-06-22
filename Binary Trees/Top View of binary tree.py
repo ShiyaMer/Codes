e167656d2a3d89e24ddc89408c992256d2b8a7b7
ShiyaMer <95877906+ShiyaMer@users.noreply.github.com>
2024-06-22 12:49:47 +0530

    Create Top View of binary tree.py

---
+++ Binary Trees/Top View of binary tree.py
@@ -0,0 +1,27 @@
+class Solution:
+    #i need the first all x cordinates in tree
+    #Function to return a list of nodes visible from the top view 
+    #from left to right in Binary Tree.
+    def topView(self,root):
+        if not root:
+            return []
+        q=[[root,0,0]]
+        d={}
+        while(q):
+            i=len(q)
+            for _ in range(i):
+                n=q.pop(0)
+                node=n[0]
+                x=n[1]
+                y=n[2]
+                if x not in d:
+                    d[x]=node.data
+                if node.left:
+                    q.append([node.left,x-1,y+1])
+                if node.right:
+                    q.append([node.right,x+1,y+1])
+        ans=[]
+        for i in sorted(d.keys()):
+            ans.append(d[i])
+        return ans    
+        
