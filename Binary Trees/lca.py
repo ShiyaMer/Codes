1f3f37f038fbb41c9c8e3c17199b4e643054696c
ShiyaMer <95877906+ShiyaMer@users.noreply.github.com>
2024-06-23 20:56:49 +0530

    Create lca.py

---
+++ Binary Trees/lca.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        #Brute
+        def findpath(roo,arr,num):
+            if not roo:
+                return False
+            arr.append(roo)
+            if roo.val ==num.val:
+                return True
+            if findpath(roo.left,arr,num) or findpath(roo.right,arr,num):
+                return True
+            arr.pop(-1)
+            return False
+        arr1=[]
+        findpath(root,arr1,p)
+        arr2=[]
+        findpath(root,arr2,q) 
+        i=0
+        node=TreeNode()
+        while(i<len(arr1) and i<len(arr2)):
+            if arr1[i]!=arr2[i]:
+                break
+            node=arr1[i]    
+            i+=1
+        return node
