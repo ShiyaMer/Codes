9146ac8345f21208b4061a59de404ea0ca889e5e
ShiyaMer <95877906+ShiyaMer@users.noreply.github.com>
2024-06-23 21:23:02 +0530

    Update lca.py

--- Binary Trees/lca.py
+++ Binary Trees/lca.py
@@ -29,3 +29,27 @@ class Solution(object):
             node=arr1[i]    
             i+=1
         return node
+        #optimal
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or root==p or root==q:
+            return root
+        left=self.lowestCommonAncestor(root.left, p, q)  
+        right=self.lowestCommonAncestor(root.right, p, q) 
+        if not left:
+            return right
+        elif not right:
+            return left
+        else:
+            return root    
+                           
+                        
+
+
+        
