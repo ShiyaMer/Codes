0c9d41224103de65b5bf631f894acacaf487432b
ShiyaMer <95877906+ShiyaMer@users.noreply.github.com>
2024-06-24 15:18:32 +0530

    Update Minimum time to burn a tree.py

--- Binary Trees/Minimum time to burn a tree.py
+++ Binary Trees/Minimum time to burn a tree.py
@@ -1,4 +1,6 @@
-if not root and  not target:
+class Solution:
+    def minTime(self, root,target):
+        if not root and  not target:
             return 0
         q0=[root]
         d={root:None}
@@ -6,6 +8,8 @@ if not root and  not target:
             i=len(q0)
             for _ in range(i):
                 n=q0.pop(0)
+                if n.data==target:
+                    target=n
                 if n.left:
                     d[n.left]=n
                     q0.append(n.left)
@@ -14,7 +18,7 @@ if not root and  not target:
                     q0.append(n.right)
         visited=[]
         q=[target]
-        dis=0
+        dis=-1#as the first node is not counted
         while(q):
             i=len(q)
             l=[]
@@ -33,3 +37,29 @@ if not root and  not target:
                     visited.append(n.right)
             dis+=1
         return dis  
+#Also possible approach:
+you can also try it by slighly different approach. After making parent map, instead of taking another bfs to find the time, You can find the height  of tree  using dfs cosidering target node as root node and also taking the help of visited map. The code of this part is similar to find the height or bt with slight modification.
+
+Code: 
+int height(Node* root ,   unordered_map<Node*,Node*>&par ,  unordered_map<Node*, int>&vis)
+{
+    if(!root)
+    return 0;
+    
+    vis[root]=1;
+    
+    int lh= INT_MIN;
+     int rh= INT_MIN;
+      int ph= INT_MIN;
+    
+    if(!vis[root->left])
+     lh= height(root->left, par, vis);
+      if(!vis[root->right])
+        rh= height(root->right, par, vis);
+          if(!vis[par[root]])
+        ph= height(par[root] , par, vis);
+    
+    return  max(ph, max(lh,rh)) +1;
+}
+
+The final ans will be height-1;
